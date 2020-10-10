e5079c74c87286b6655c73f4d189f769575e6ed8
Crowdbotics <team@crowdbotics.com>
2020-10-10 20:25:02 +0000

    Update models

--- backend/one_reliable_courie_21343/wsgi.py
+++ backend/one_reliable_courie_21343/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_reliable_courie_21343.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "one_reliable_courie_21343.settings")
 
 application = get_wsgi_application()
